6576723ac3917d8bbe1d488c18fe9e168c9ca389
lstasiuk <leanne.stasiuk@gmail.com>
2018-07-20 11:33:38 -0700

    partial files set

--- build/js/jscript.js
+++ build/js/jscript.js
@@ -1,44 +1,33 @@
 $(document).ready(function() {
 
-            $("#sections").on("change", function() {
+    $("#sections").on("change", function() {
 
-                    var selectedStory = $("#sections").val();
+        var selectedStory = $("#sections").val();
 
-                    $('#appended-stories').empty();
+        $('#appended-stories').empty();
 
-                    var url = 'https://api.nytimes.com/svc/topstories/v2/' + selectedStory + '.json';
-                    url += '?' + $.param({
-                        'api-key': "b340bf0706784521880392a9f328b350"
-                    });
+        var url = 'https://api.nytimes.com/svc/topstories/v2/' + selectedStory + '.json';
+        url += '?' + $.param({
+            'api-key': "b340bf0706784521880392a9f328b350"
+        });
 
-                    $.ajax({
-                            url: url,
-                            method: 'GET',
-                        })
-                        .done(function(data) {
-                                console.log(data.results);
-                                $.each(data.results, function(key, value) {
-                                        $("appended-stories").append("<div class='articles'>" + "<a href=" + value.url + "></a>" + "<img src=" + value.multimedia[4].url + ">" + "<p>" + value.abstract + "</p>" + "</div>"
+        $.ajax({
+                url: url,
+                method: 'GET',
+            })
+            .done(function(data) {
+                console.log(data.results);
+                $.each(data.results, function(key, value) {
+                    $("appended-stories").append("<div class='articles'>" + "<a href=" + value.url + "></a>" + "<img src=" + value.multimedia[4].url + ">" + "<p>" + value.abstract + "</p>" + "</div>")
+                        ("#appended-stories").append(
+                            console.log(data.results)
 
-
-
-                                            // var html += "<div class='
-
-
-
-
-
-                                            // html += "</div>";
-                                            ("#appended-stories").append(
-
-
-                                            )
-                                        });
-                                    // var resultsArray = data.results;
-                                })
-                            .fail(function(err) {
-                                throw err;
-                            })
-                            .always(function() {});
-                        });
-            });
+                        )
+                });
+            })
+            .fail(function(err) {
+                throw err;
+            })
+            .always(function() {});
+    });
+});
