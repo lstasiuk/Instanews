59c424373f4b6400da5e5809fad82ae9db396ae4
lstasiuk <leanne.stasiuk@gmail.com>
2018-07-20 16:30:45 -0700

    filter and slice added

--- build/js/jscript.js
+++ build/js/jscript.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
 
     $("#sections").on("change", function() {
+        $('header').addClass("active")
         $("#appended-stories").empty();
         var selectedStory = $("#sections").val();
+
+        $("#header-nav").addClass("articles-loaded");
 
         var url = 'https://api.nytimes.com/svc/topstories/v2/' + selectedStory + '.json';
         url += '?' + $.param({
@@ -14,8 +17,13 @@ $(document).ready(function() {
                 method: 'GET',
             })
             .done(function(data) {
-                console.log(data.results);
-                $.each(data.results, function(key, value) {
+                var images = data.results
+                    .filter(function(result) {
+                        return result.multimedia.length;
+                    })
+                    .slice(0, 12);
+
+                $.each(images, function(key, value) {
                     $("#appended-stories").append("<div class='articles'>" + "<a target='_blank' href=" + value.url + ">" + "<img class='images' src=" + value.multimedia[4].url + ">" + "<div class='abstract' <p>" + value.abstract + "</p></div>" + "</a>" + "</div>")
 
 
